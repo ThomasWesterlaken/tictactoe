38f456aa159a7e7c4574effa4385cc8b82a06d05
commandstring <bmxmikey2@gmail.com>
2023-03-02 09:40:52 -0500

    Even more code styling improvements

--- codebase/tictactoe.js
+++ codebase/tictactoe.js
@@ -1,6 +1,6 @@
 let vakjes = [] // collection of table details
 let gameactive = false
-let currsymbol = "X"
+let currSymbol = "X"
 let music = new Audio('musicbackground.mp3')
 music.loop = true
 
@@ -24,80 +24,79 @@ for (let x = 0; x < 3; x++) {
         
         cell.style.backgroundColor = (x % 2 !== y % 2) ? "grey" : "lightgrey"
 
-        row.appendChild(cell); // add row to DOM
-        vakjes.push(cell); // adds cells to #vakjes
+        row.appendChild(cell) // add row to DOM
+        vakjes.push(cell) // adds cells to #vakjes
     }
 
-    document.querySelector('#bord').appendChild(row);  // selects css of bord
+    document.querySelector('#bord').appendChild(row)  // selects css of bord
 }
 
 document.querySelectorAll('.symbolfactory span').forEach(symbol => {
     symbol.addEventListener('dragstart', (e) => {
-        e.dataTransfer.setData('symbol', symbol.innerText);
+        e.dataTransfer.setData('symbol', symbol.innerText)
     })
 })
 
 vakjes.forEach(vakje => {
-    vakje.addEventListener('dragover', (e) => e.preventDefault());
-    vakje.addEventListener('drop', (e) => dropTicOfTac(e));
+    vakje.addEventListener('dragover', (e) => e.preventDefault())
+    vakje.addEventListener('drop', (e) => dropTicOfTac(e))
 })
 
-musicPlayer();
+musicPlayer()
 
-function musicPlayer() { // functions should be either camalCase
-    music.volume = 0.15;
-    music.play();
-    setTimeout(music.play(), 500);
+function musicPlayer() {
+    music.volume = 0.15
+    music.play()
 }
 
 function dropTicOfTac(event) {
-    let symbol = event.dataTransfer.getData('symbol');
-    let cell = event.target;
-    music.play();
-    if (currsymbol != symbol) {
-        alert(`The current player is ${currsymbol} but you tried to place ${symbol}`);
-        return;
+    let symbol = event.dataTransfer.getData('symbol')
+    let cell = event.target
+    music.play()
+    if (currSymbol != symbol) {
+        alert(`The current player is ${currSymbol} but you tried to place ${symbol}`)
+        return
     }
 
-    if (!validatemove(cell)) {
-        alert("You are not allowed to place a move here");
-        return;
+    if (!validateMove(cell)) {
+        alert("You are not allowed to place a move here")
+        return
     }
 
-    cell.innerText = symbol;
+    cell.innerText = symbol
 
-    if (currsymbol == "X") {
-        currsymbol = "O";
+    if (currSymbol == "X") {
+        currSymbol = "O"
     } else {
-        currsymbol = "X";
+        currSymbol = "X"
     }
 
-    let haswinnerresult = haswinner();
+    let hasWinnerResult = hasWinner()
 
-    if (haswinnerresult != null) {
-        colorin(haswinnerresult[0]);
-        setwinmessage(haswinnerresult[1])
+    if (hasWinnerResult != null) {
+        colorIn(hasWinnerResult[0])
+        setWinMessage(hasWinnerResult[1])
     }
 }
 
-function validatemove(square) {
-    return square.innerText == "" ? true : false;
+function validateMove(square) {
+    return square.innerText == "" ? true : false
 }
 
-function haswinner() {
+function hasWinner() {
     for (i = 0; i < winningConditions.length; i++) {
-        t = winningConditions[i];
-        let result = checksequence(t);
+        t = winningConditions[i]
+        let result = checkSequence(t)
         
         if (result == null) {
-            continue;
+            continue
         } else {
             return [t, result]
         }
     }
 }
 
-function checksequence(t) {
+function checkSequence(t) {
     if (vakjes[t[0]].innerText == "X" && vakjes[t[1]].innerText == "X" && vakjes[t[2]].innerText == "X") {
         return "X"
     } else if (vakjes[t[0]].innerText == "O" && vakjes[t[1]].innerText == "O" && vakjes[t[2]].innerText == "O") {
@@ -105,12 +104,12 @@ function checksequence(t) {
     }
 }
 
-function colorin(sequence) {
+function colorIn(sequence) {
     for (i = 0; i < sequence.length; i++) {
-        vakjes[sequence[i]].style.backgroundColor = "red";
+        vakjes[sequence[i]].style.backgroundColor = "red"
     }
 }
 
-function setwinmessage(winner) {
-    document.querySelector('.winnermessage span').innerHTML = `Congratiulations ${winner} has won the game!`;
+function setWinMessage(winner) {
+    document.querySelector('.winnermessage span').innerHTML = `Congratiulations ${winner} has won the game!`
 }
